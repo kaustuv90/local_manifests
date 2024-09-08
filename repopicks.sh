be8e36cf2611c82a85ce3649944046dfc1972e00
OGGY <42085276+kaustuv90@users.noreply.github.com>
2024-09-08 13:31:33 +0530

    Update repopicks.sh
    Added kernal SU support

--- repopicks.sh
+++ repopicks.sh
@@ -30,3 +30,10 @@ changes=(
 repopick -g https://review.lineageos.org -P vendor/lineage ${changes[@]}&
 
 wait
+
+# Build kernel with KernelSU from main branch
+cd kernel/xiaomi/sm8450
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+git add --all
+git commit -m "Build kernel with KernelSU from main branch"
+cd ../../..
