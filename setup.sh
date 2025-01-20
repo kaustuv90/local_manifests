b143f28b38cb567fe57a3dd378037ea811fd5831
Kaustuv Mahanti <42085276+kaustuv90@users.noreply.github.com>
2025-01-20 14:05:23 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ set -e
 source "build/envsetup.sh";
 source "vendor/lineage/build/envsetup.sh";
 
+# vendor/lineage
+changes=(
+367044 # android: merge_dtbs: Respect miboard-id while merging
+)
+repopick -P vendor/lineage ${changes[@]}&
+
 wait
 
 # Build kernel with KernelSU from main branch
